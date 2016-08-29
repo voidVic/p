e033045e49402cdcd4115755e538b3ab38177fa4
Ankit Sharma <vicnwz@gmail.com>
2016-08-29 17:27:28 +0530

    Update app.js

--- bin/app.js
+++ bin/app.js
@@ -4,5 +4,5 @@ var app = require('../server');
 app.set('port', process.env.PORT || 9299);
 
 var server = app.listen(app.get('port'), function() {
-  console.log('Server listening on port ' + server.address().port);
+  //console.log('Server listening on port ' + server.address().port);
 });
