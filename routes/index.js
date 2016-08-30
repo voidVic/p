769a9d857cfdf94fc3b6f6742c3878425aba9bb9
Ankit Sharma <vicnwz@gmail.com>
2016-08-30 10:47:25 +0530

    removing mailservice injection

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var mailService = require('../appService/mailService').mail;
+//var mailService = require('../appService/mailService').mail;
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Ankit' });
